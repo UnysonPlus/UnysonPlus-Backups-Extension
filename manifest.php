88743e07f2abc1985b72d105a77a1f3f7b85542d
jonmlas <jonmlas@gmail.com>
2025-08-21 18:46:28 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -1,26 +1,40 @@
 <?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
+    die( 'Forbidden' );
 }
 
 $manifest = array();
 
-$manifest['name']        = __( 'Backup & Demo Content', 'fw' );
+// Basic Info
+$manifest['name']        = __( 'Backup & Demo Content', 'unysonplus' );
+$manifest['slug']        = 'unysonplus-backup-content';
 $manifest['description'] = __(
-	'This extension lets you create an automated backup schedule,'
-	.' import demo content or even create a demo content archive for migration purposes.',
-	'fw'
-);
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Backups-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/backups/index.html';
-$manifest['version'] = '2.0.33';
-$manifest['display'] = true;
-$manifest['standalone'] = true;
-$manifest['requirements'] = array(
-	'framework' => array(
-		'min_version' => '2.6.16',
-	),
+    'This extension lets you create an automated backup schedule, import demo content, or even create a demo content archive for migration purposes.',
+    'unysonplus'
 );
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-Backups-Extension';
+$manifest['version']     = '2.0.33';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
+
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Backups-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Backups-Extension';
+$manifest['github_branch'] = 'master';
+$manifest['uri']           = 'http://manual.unyson.io/en/latest/extension/backups/index.html';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Requirements
+$manifest['requirements'] = array(
+    'framework' => array(
+        'min_version' => '2.6.16',
+    ),
+);
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'unysonplus';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
